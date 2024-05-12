58f372c9fcc009697b3267d763320fdf9df34b66
Olumide Omitiran <olumide.omitiran@thepfs.biz>
2024-05-13 00:46:26 +0100

    frontend now has QR code when wifi details is put

--- README.old.md
+++ README.old.md
@@ -1,2 +1,16 @@
 # wifiqrgenerator
 This is a application for generating a QR code for your Wi-Fi details making connection faster and easier
+
+We propose a syntax like "MECARD" for specifying wi-fi configuration. Scanning such a code would, after prompting the user, configure the device's Wi-Fi accordingly. Example:
+
+WIFI:T:WPA;S:mynetwork;P:mypass;;
+Parameter	Example	Description
+T	WPA	Authentication type; can be WEP or WPA or WPA2-EAP, or nopass for no password. Or, omit for no password.
+S	mynetwork	Network SSID. Required. Enclose in double quotes if it is an ASCII name, but could be interpreted as hex (i.e. "ABCD")
+P	mypass	Password, ignored if T is nopass (in which case it may be omitted). Enclose in double quotes if it is an ASCII name, but could be interpreted as hex (i.e. "ABCD")
+H	true	Optional. True if the network SSID is hidden. Note this was mistakenly also used to specify phase 2 method in releases up to 4.7.8 / Barcode Scanner 3.4.0. If not a boolean, it will be interpreted as phase 2 method (see below) for backwards-compatibility
+E	TTLS	(WPA2-EAP only) EAP method, like TTLS or PWD
+A	anon	(WPA2-EAP only) Anonymous identity
+I	myidentity	(WPA2-EAP only) Identity
+PH2	MSCHAPV2	(WPA2-EAP only) Phase 2 method, like MSCHAPV2
+Order of fields does not matter. Special characters \, ;, ,, " and : should be escaped with a backslash (\) as in MECARD encoding. For example, if an SSID was literally "foo;bar\baz" (with double quotes part of the SSID name itself) then it would be encoded like: WIFI:S:\"foo\;bar\\baz\";;
--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import axios from "axios";
 import "./App.css";
+import axios from "axios";
 
+interface WifiDetails {
+  ssid: string;
+  password: string;
+  encryption: "WPA" | "WEP" | "None";
+}
 
 function App() {
-  const [ssid, SetSSID] = React.useState("");
-  const [password, SetPassword] = React.useState("");
-  const [qr, SetQR] = React.useState(null);
+  const [wifiDetails, setWiFiDetails] = useState<WifiDetails>({
+    ssid: "",
+    password: "",
+    encryption: "WPA",
+  });
+  const [qrImage, setQRImage] = useState<string>("");
 
-  const generateQR = () => {
-    axios.post("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", { ssid, password })
-      .then(res => {
-        SetQR(res.data);
+  const GenerateQRCode = async (event: React.FormEvent) => {
+    event.preventDefault();
+    // console.log(wifiDetails);
+    if (wifiDetails.ssid === "" || wifiDetails.password === "") {
+      alert("Please enter Wi-Fi details");
+      return;
+    }
+
+    try {
+      await axios.post("http://localhost:8000/createqr", wifiDetails).then(res => {
+        console.log(res);
+        setQRImage(res.data.qrcode);
       })
-      .catch(err => {
-        console.log(err);
-      })
-  }
+
+    } catch (error) {
+      console.log(error)
+    }    
+
+    
+  };
 
   return (
     // <div className="App">
     <div className="container">
       <div className="row">
-        <h1 className="App display-1">Wi-Fi QR Generator</h1><i></i>
+        <h1 className="App display-1">Wi-Fi QR Code Generator</h1>
+        <i></i>
       </div>
 
-      <Row 
-        xs={1}
+      <Row
+        // xs={1}
         md={2}
         className="g-4"
         style={{ marginTop: "20px" }}
         justify="center"
-        align="center"
-        noGutters={true}
-        alignContent="center"
-        // alignItems="center"
+        // noGutters={true}
       >
         <Col>
-          <div className="card" style={{ width: "28rem" }}>
-         
+          <div className="card" style={{ width: "18rem" }}>
             <div className="card-body">
               <h5 className="card-title">Enter Details</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
+              <form>
+
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Wi-Fi Name
+                  </label>
+                  <input
+                    type="text"
+                    value={wifiDetails.ssid}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="ssidHelp"
+                    placeholder="Enter your Wi-Fi name"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    spellCheck="false"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setWiFiDetails({ ...wifiDetails, ssid: e.target.value });
+                    }}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    value={wifiDetails.password}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter your Wi-Fi paassword"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    spellCheck="false"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setWiFiDetails({
+                        ...wifiDetails,
+                        password: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+               
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEnc" className="form-label">
+                    Encryption
+                  </label>{"  "}
+                  <select onChange={
+                    (e: React.ChangeEvent<HTMLSelectElement>) => {
+                      setWiFiDetails({
+                        ...wifiDetails,
+                        encryption: e.target.value as WifiDetails["encryption"],
+                      });
+                    }
+                  }>
+                    <option value="WPA">WPA/WPA2/WPA3</option>
+                    <option value="WEP">WEP</option>
+                    <option value="None">None</option>
+                  </select>
+
+                </div>
+              </form>
+
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={{ marginTop: "10px" }}
+                onClick={GenerateQRCode}
+              >
+                Generate QR Code
+              </button>
             </div>
           </div>
         </Col>
 
-        <Col xs={5}>
-          <h5 className="card-title">QR Code</h5>
+        <Col md={5}>
+
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">QR Code</h5>
+              <div className="card-text">
+
+                { qrImage && <img src={qrImage} alt="QR Image" style={{ width: "100%", height: "100%" }}  /> }
+              </div>
+            </div>
+          </div>
 
         </Col>
       </Row>
@@ -65,3 +157,4 @@ function App() {
 }
 
 export default App;
+
